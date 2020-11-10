c1eda232d4e5fa73c555f09c7c7689a5a2a725e7
kicok <kicoker@gmail.com>
2020-11-10 17:46:51 +0900

    Sending & Ordering Chat Messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../widgets/chat/new_message.dart';
 import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -42,16 +43,9 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          FirebaseFirestore.instance
-              .collection('chats/mNJDKVVbYkMUaZwwTueu/messages')
-              .add({'text': 'This was added by clicking the button!'});
-        },
       ),
     );
   }
